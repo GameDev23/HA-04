6e793c2bfa25acef2b008b232ac7e11a42ce2847
HerrKermet <david.stechow1113@gmail.com>
2023-05-10 21:53:41 +0200

    Added manager objects

---
+++ Assets/Manager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    #region Global variables
+    //Declare global variables here and make them static
+    public static int test;
+    #endregion
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
