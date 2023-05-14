0eaef274d737cdd51fe92dbcad5df30982a075a2
HerrKermet <david.stechow1113@gmail.com>
2023-05-14 23:09:31 +0200

    Finished Task

---
+++ Assets/DisableHitboxOnDeath.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableHitboxOnDeath : MonoBehaviour
+{
+    private Collider2D myCollider;
+
+    private bool isHitboxActive = true;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        myCollider = gameObject.GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (!Manager.Instance.isAlive && isHitboxActive)
+        {
+            myCollider.enabled = false;
+            isHitboxActive = false;
+        }
+    }
+}
--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    private CapsuleCollider2D myCollider;
 
-    private bool isHitboxActive = true;
     
     // Start is called before the first frame update
     private void Start()
     {
-        myCollider = gameObject.GetComponent<CapsuleCollider2D>();
+
     }
 
     // Update is called once per frame
@@ -20,11 +18,5 @@ public class EnemyMovement : MonoBehaviour
     {
         if(Manager.Instance.isSection2)
             transform.position += new Vector3(-6 * Time.deltaTime, 0 , 0);
-        
-        if (!Manager.Instance.isAlive && isHitboxActive)
-        {
-            myCollider.enabled = false;
-            isHitboxActive = false;
-        }
     }
 }
