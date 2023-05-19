f8cc2316207af25420ccddfc92064fa67fef3ac8
MarvinKha <103587829+MarvinKha@users.noreply.github.com>
2023-05-19 16:38:49 +0200

    added MikuMikuMikuMikuMikuMikuMikuMikuMikuMiku
    MikuMikuMikuMikuMikuMikuMikuMikuMikuMikuMikuMikuMikuMikuMikuMikuMikuMikuMikuMikuMikuMikuMikuMikuMikuMikuMikuMiku

--- Assets/David/PlayerScript.cs
+++ Assets/David/PlayerScript.cs
@@ -133,7 +133,9 @@ public class PlayerScript : MonoBehaviour
             if ((Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Mouse0)))
             {
                 Debug.Log("Reset Scene");
-                SceneManager.LoadScene("DavidLevel");
+                Scene scene = SceneManager.GetActiveScene();
+                SceneManager.LoadScene(scene.name);
+                
             }
         }
 
